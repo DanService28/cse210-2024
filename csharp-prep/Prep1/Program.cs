0e7231f2f7adb817a77851d1ab065879f56df000
DanService28 <danherrera2708@gmail.com>
2024-09-21 21:13:35 -0600

    First and Last name

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.Write("What's your name?: ");
+        string name = Console.ReadLine();
+        Console.Write("What's your last name?: ");
+        string last_name = Console.ReadLine();
+
+        Console.WriteLine ($"Your name is {last_name}, {name} {last_name}.");
+        
     }
 }
