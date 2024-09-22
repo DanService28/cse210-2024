953556067aebb8417ff4d76e7d238fffb19a740a
DanService28 <danherrera2708@gmail.com>
2024-09-21 23:36:12 -0600

    Magic number program

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomNumber = new Random();
+        int number = randomNumber.Next(1, 100);
+
+        int guessedNumber = -1;
+
+        while (number != guessedNumber)
+        {
+            Console.Write("Guess a number among 1-100? ");
+            guessedNumber = int.Parse(Console.ReadLine());
+
+            if (number > guessedNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (number < guessedNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You got it!");
+            }
+        }
     }
 }
