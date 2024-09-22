295bae56b600f62da06c689f4d92141c7dd8bc37
DanService28 <danherrera2708@gmail.com>
2024-09-21 23:03:29 -0600

    if statements on grades excersice

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,39 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        //int A >= 90;
+        //int B >= 80;
+        //int C >= 70;
+        //int D >= 60;
+        //int F <= 60;
+
+        Console.Write("How much is your grade percentage?: ");
+        string userGradeSt = Console.ReadLine();
+        int userGrade = int.Parse(userGradeSt);
+
+        string letter = "";
+
+        if (userGrade >= 90 && userGrade <= 100)
+           {
+                letter = "A";
+           }
+        else if (userGrade >= 80 && userGrade <= 90)
+            {
+                letter = "B";
+            }
+            
+        else if (userGrade >= 70 && userGrade <= 80 )
+            {
+                letter = "C";
+            }
+        else if (userGrade >= 60 && userGrade <= 70 )
+            {
+                letter = "D";
+            }   
+        else 
+            {
+                letter = "F";
+            }
+        Console.WriteLine($"Your final grade is {letter}");
     }
 }
