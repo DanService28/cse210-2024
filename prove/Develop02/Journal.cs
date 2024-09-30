8bc9d01350231f486354bfac6532c9ba3ca1ca64
DanService28 <danherrera2708@gmail.com>
2024-09-30 00:19:23 -0600

    loadfile

--- prove/Develop02/Journal.cs
+++ prove/Develop02/Journal.cs
@@ -30,7 +30,7 @@ public class Journal
         {   
             foreach(Entry entry in _entries)
             {
-                outputFile.WriteLine($"{entry._date}----{entry._promptText}---{entry._entryText}");
+                outputFile.WriteLine($"{entry._date}---{entry._promptText}---{entry._entryText}");
             }
         }
     }
@@ -41,8 +41,14 @@ public class Journal
 
         foreach (string line in lines)
         {
+            Entry entry = new Entry();
             string[] parts = line.Split("---");
             
+            entry._date = parts[0];
+            entry._promptText = parts[1];
+            entry._entryText = parts[2];
+
+            AddEntry(entry);
         }
     }
     
